88d074010c5724579aa0bf78d6d64595abc2da8b
HamzaHersi01 <Hamza_Hersi01@hotmail.com>
2023-11-12 23:13:20 +0000

    created functions for add, sub, div, multiply

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+inputs = {
+    num1:null,
+    operand: null,
+    num2: null
+}
 
+function add(x, y){
+    return x+y
+}
+    
+
+function sub(x, y){
+    return x-y
+}
+
+function mult(x, y){
+    return x*y
+}
+
+function div(x, y){
+    return x/y
+}
