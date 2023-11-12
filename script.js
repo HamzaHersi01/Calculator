aefe79cdb8408c158e456efd1cbb8947ce53ab65
HamzaHersi01 <Hamza_Hersi01@hotmail.com>
2023-11-12 23:18:42 +0000

    created calcualte function that determines which fucntion to return via switch statement

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ function mult(x, y){
 function div(x, y){
     return x/y
 }
+
+function calculate(inputs){
+    switch(inputs.operand) {
+        case '+':
+            return add(inputs.num1, inputs.num2);
+        case '-':
+            return sub(inputs.num1, inputs.num2);
+        case '*':
+            return mult(inputs.num1, inputs.num2);
+        case '/':
+            return div(inputs.num1, inputs.num2);
+        default:
+            return "Invalid operand";
+    }
+}
