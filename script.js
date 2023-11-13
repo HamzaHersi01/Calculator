829ddb0fb0fcfcc08bc0273b2c6eb58a372fd5d9
HamzaHersi01 <Hamza_Hersi01@hotmail.com>
2023-11-13 20:44:40 +0000

    Added delete fuctionality

--- script.js
+++ script.js
@@ -4,21 +4,36 @@ inputs = {
     num2: null,
     sum: null,
     previousSum: null,
+    lastKey: null,
 };
 
+const currentCal = document.querySelector(".currentCal");
+
 function add(x, y) {
+    if(y === null){
+        y = 0;
+    }
     return x + y;
 }
 
 function sub(x, y) {
+    if(y === null){
+        y = 0;
+    }
     return x - y;
 }
 
 function mult(x, y) {
+    if(y === null){
+        y = 0;
+    }
     return x * y;
 }
 
 function div(x, y) {
+    if(y === null){
+        y = 0;
+    }
     return x / y;
 }
 
@@ -35,17 +50,19 @@ function clear(){
     inputs.operand = null;
     inputs.num2 = null;
     inputs.sum = null;
-    document.querySelector(".currentCal").textContent = ` `;
+    currentCal.textContent = ` `;
 }
+
+
 function updateDisplay() {
     if (inputs.operand === null && inputs.num2 === null) {
-        document.querySelector(".currentCal").textContent = `${inputs.num1} `;
+        currentCal.textContent = `${inputs.num1} `;
         console.log("Num1");
     } else if (inputs.operand != null && inputs.num2 === null) {
-        document.querySelector(".currentCal").textContent = `${inputs.num1} ${inputs.operand}`;
+        currentCal.textContent = `${inputs.num1} ${inputs.operand}`;
         console.log("operand");
     } else {
-        document.querySelector(".currentCal").textContent = `${inputs.num1} ${inputs.operand} ${inputs.num2}`;
+        currentCal.textContent = `${inputs.num1} ${inputs.operand} ${inputs.num2}`;
     }
 }
 
@@ -53,6 +70,7 @@ function appendInput(key, number) {
     if (key === "num1") {
         if (inputs.num1 === null) {
             inputs.num1 = number;
+            inputs.lastKey= 'num1'
             updateDisplay();
         } else {
             inputs.num1 = String(inputs.num1 + number);
@@ -61,6 +79,7 @@ function appendInput(key, number) {
     } else {
         if (inputs.num2 === null) {
             inputs.num2 = number;
+            inputs.lastKey= 'num2'
             updateDisplay();
         } else {
             inputs.num2 = String(inputs.num2 + number);
@@ -100,6 +119,7 @@ function init() {
         operand.addEventListener("click", function (event) {
             if(inputs.num1 != null){
             inputs.operand = event.target.id;
+            inputs.lastKey= 'operand'
             updateDisplay();
             }
         });
@@ -109,14 +129,57 @@ function init() {
     equals.addEventListener("click", function () {
         inputs.num1 = Number(inputs.num1);
         inputs.num2 = Number(inputs.num2);
+        if(inputs.num1 && inputs.num2 && inputs.operand){
         inputs.sum = calculate(inputs);
-        document.querySelector(".currentCal").textContent = inputs.sum;
+        currentCal.textContent = inputs.sum;
+        inputs.lastKey='num1';
         formatDisplay();
+        console.log(inputs)
+        }
     });
 
     document.querySelector('.clear').addEventListener('click',function(){
         clear();
-    })
+    });
+
+    document.querySelector('.delete').addEventListener('click', function() {
+        if (inputs.lastKey) {
+            console.log("Delete");
+            console.log(inputs.lastKey);
+            switch (inputs.lastKey) {
+                case 'num2':
+                    let number2 = inputs.num2;
+                    number2 = number2.split("");
+                    if (number2.length > 0) {
+                        number2.pop();
+                        inputs.num2 = number2.join('');
+                        updateDisplay();
+                    } else {
+                        inputs.num2 = null;
+                        inputs.lastKey = 'operand';
+                    }
+                    break;
+                case 'operand':
+                    if (inputs.operand !== null) {
+                        inputs.operand = null;
+                        updateDisplay();
+                    } else {
+                        inputs.lastKey = 'num1';
+                    }
+                    break;
+                case 'num1':
+                    console.log("Case1");
+                    let number1 = inputs.num1;
+                    number1 = number1.split("");
+                    if (number1.length > 0) {
+                        number1.pop();
+                        inputs.num1 = number1.join('');
+                        updateDisplay();
+                    }
+                    break;
+            }
+        }
+    });
 }
 
 init();
